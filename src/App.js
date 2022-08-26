ed50357543a6cb7b693286aab4405805b413bc83
lovinskr <55713318+lovinskr@users.noreply.github.com>
2022-08-25 22:25:13 -0400

    Add: First Change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey I did it. This is Kris' React app!
         </p>
         <a
           className="App-link"
